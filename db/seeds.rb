a037d3fa6e81c6d1cad0215e3da4c0da1b8339a6
Steven Le Moal <steven.lemoal@gmail.com>
2020-11-11 09:05:52 +0100

    Yelp MVP, need to implement review display in the show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create!([{ name: 'Commis', address: '3859 Piedmont Ave, Oakland, CA 94609, United States', phone_number: '510-653-3902', category: 'french' },
+                                 { name: 'Tacos Oscar', address: '420 40th St, Oakland, CA 94609, United States', phone_number: '510-735-9620', category: 'belgian' },
+                                 { name: 'Monster Pho', address: '3905 Broadway, Oakland, CA 94611, United States', phone_number: '510-788-4459', category: 'japanese' },
+                                 { name: 'Belotti Ristorante E Bottega', address: '5403 College Ave, Oakland, CA 94618, United States', phone_number: '510-788-7890', category: 'italian' },
+                                 { name: 'Lucias', address: '2016 Shattuck Ave, Berkeley, CA 94704, United States', phone_number: '510-225-9467', category: 'italian' }])
